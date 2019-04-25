401c1316cf9196edf028378b95652dd8ab0dbbf1
lixiong <2749811373@qq.com>
2019-04-25 19:54:13 +0800

    acheve string

---
+++ other_code/4_25_test/string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include<string>
+int main()
+{
+  string s1("hello world");
+  string s2(s1);
+  string s3;
+  string s4(10,'a');
+
+  cout<<s1.size()<<endl;
+  return 0;
+}
