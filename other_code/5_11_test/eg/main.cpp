a795e85781e11584dcf13bf6612b48a64222f037
lixiong <2749811373@qq.com>
2019-05-11 23:41:26 +0800

    解决乱码

--- other_code/5_11_test/eg/main.cpp
+++ other_code/5_11_test/eg/main.cpp
@@ -4,10 +4,10 @@
 int main() {
     MyDb db;
 
-    string host = "127.0.0.1";
+    string host = "47.103.3.230";
     string user = "root";
     string passwd = "password";
-    string dbName = "test";
+    string dbName = "oj_model";
     int port = 3306;
 
     cout<<"start\n";
@@ -21,11 +21,13 @@ int main() {
 
     cout<<"ok" <<endl;
 
-    //将用户信息添加到数据库
-    string sql = "INSERT account values('1', 'fengxin');";
-    sql = "INSERT account values('2', 'axin');";
-    //将所有用户信息读出，并输出。
-    sql = "SELECT * from account;";
+//    //将用户信息添加到数据库
+//    string sql = "INSERT account values('1', 'fengxin');";
+//    sql = "INSERT account values('2', 'axin');";
+//    //将所有用户信息读出，并输出。
+    std::string sql = "set names utf8";
+    db.exeSQL(sql);
+    sql = "SELECT * from question;";
     db.exeSQL(sql);
 
     return 0;
