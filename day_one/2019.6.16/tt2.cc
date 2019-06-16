ab72fb3056cf272abdad38ef55890eea5d413849
lixiong <2749811373@qq.com>
2019-06-16 10:58:53 +0800

    code

---
+++ day_one/2019.6.16/tt2.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void help(int n, int m, vector<int>& v, int beg) {
+    //if (beg>n) return;
+    if (m == 0) {
+        for (int i = 0; i<v.size(); i++) {
+            i == 0 ? cout << v[i] : cout << " " << v[i];
+        }
+        cout << endl;
+    }
+    for(auto i:v)
+        cout<<i<<" ";
+    cout<<endl;
+    for (int i = beg; i <= n&&i <= m; i++) {
+        v.push_back(i);
+        help(n, m - i, v, i + 1);
+        v.pop_back();
+    }
+}
+int main() {
+    int n, m;
+    while (cin >> n >> m) {
+        vector<int>v;
+        help(n, m, v, 1);
+    }
+}
