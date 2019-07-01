1118bb7d95e3b5f7776991053b9c38e57d18c368
lixiong <2749811373@qq.com>
2019-07-01 22:02:26 +0800

    code

---
+++ day_one/2019.7.1/question_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include<math.h>
+class Solution {
+    public:
+        int jumpFloorII(int number) {
+            return pow(2,number-1);
+
+        }
+
+};
+
+int main()
+{
+     
+    return 0;
+}
