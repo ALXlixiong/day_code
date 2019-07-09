c2fdbb9c199e4e5cb8b045585b5c8319212d702b
lixiong <2749811373@qq.com>
2019-07-09 22:47:11 +0800

    类封装实现AVL树

--- other_code/2019.7.9/avl_tree/avl.cpp
+++ other_code/2019.7.9/avl_tree/avl.cpp
@@ -7,8 +7,9 @@ struct AVLTreeNode
     AVLTreeNode<K,V> *left_;
     AVLTreeNode<K,V> *parent_;
     std::pair<K,V> val_;
+    int bf_;
 
-    AVLTreeNode(K first,V second):val_(first,second),right_(nullptr),left_(nullptr),parent_(nullptr)
+    AVLTreeNode(K first,V second):val_(first,second),bf_(0),right_(nullptr),left_(nullptr),parent_(nullptr)
     {}
 };
 
@@ -25,6 +26,56 @@ class AVLTree
 
         bool insert(K first,V second)
         {
+            if(Root_ == nullptr)
+            {
+                Root_ = new Node(first,second);
+                return true;
+            }
 
+            PNode pCur = Root_;
+            pNode pParent = nullptr;
+            while(pCur)
+            {
+                pParent = pCur;
+                if(pCur->val_.first == first)
+                    return false;
+                else if(first>pCur->val_.first)
+                    pCur = pCur->right_;
+                else
+                    pCur = pCur->left_;
+            }
+            pCur  = new Node(first,second);
+            if(pParent->val_.first > first)
+            {
+               pParent->left_ = pCur;
+               pCur->parent_ = pParent;
+            }
+            else
+            {
+                pParent->right_ = pCur;
+                pCur->parent_ = pParent;
+            }
+
+            while(pParent)
+            {
+               if(pParent->left_ == pCur)
+                   pParent->bf_--;
+               else
+                   pParent->bf_++;
+
+               if(pParent->bf_ == 0)
+                   break;
+               else if(pParent->bf_ == -1 || pParent->bf_ == 1)
+               {
+                   pCur = pParent;
+                   pParent = pParent->parent_;
+               }
+               else if(pParent->bf_ == -2 || pParent->bf_ == 2)
+               {
+
+               }
+            }
         }
-}
+    private:
+        
+};
