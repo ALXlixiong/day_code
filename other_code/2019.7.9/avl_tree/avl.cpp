1a3948d6c7640b4866dd1ea45e4bc6a582dcb2bf
lixiong <2749811373@qq.com>
2019-07-10 13:00:07 +0800

    类封装实现AVL树

--- other_code/2019.7.9/avl_tree/avl.cpp
+++ other_code/2019.7.9/avl_tree/avl.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 template<class K,class V>
 struct AVLTreeNode
@@ -33,7 +34,7 @@ class AVLTree
             }
 
             PNode pCur = Root_;
-            pNode pParent = nullptr;
+            PNode pParent = nullptr;
             while(pCur)
             {
                 pParent = pCur;
@@ -72,15 +73,29 @@ class AVLTree
                }
                else if(pParent->bf_ == -2 || pParent->bf_ == 2)
                {
-
+                   if(pParent->bf_ == 2)
+                   {
+                       if(pParent->left_->bf_ == 1)
+                           RotateLeft(pParent);
+                       else
+                           RightRotateLeft(pParent);
+                   }
+                   else
+                   {
+                       if(pParent->right_->bf_ == -1)
+                           RotateRight(pParent);
+                       else
+                           LeftRotateRight(pParent);
+                   }
                }
             }
+            return true;
         }
     private:
         void RotateRight(PNode &pParent)
         {
             PNode pSubL = pParent->left_;
-            pNode pSubLR = pSubL->right_;
+            PNode pSubLR = pSubL->right_;
 
             PNode ppParent = pParent->parent_;
 
@@ -106,14 +121,54 @@ class AVLTree
         }
         void RotateLeft(PNode &pParent)
         {
-            ;
+            PNode pSubR = pParent->right_;
+            PNode pSubRL = pSubR->left_;
+
+            PNode ppParent = pParent->parent_;
+
+            if(pSubRL != nullptr)
+                pSubRL->parent_ = pParent;
+            pParent->left_ = pSubRL;
+            pParent->parent_ = pSubR;
+            pSubR->left_ = pParent;
+
+            if(ppParent == nullptr)
+            {
+                Root_ = pSubR;
+            }
+            else
+            {
+                if(ppParent->left_ == pParent)
+                    ppParent->left_ = pSubR;
+                else
+                    ppParent->right_ = pSubR;
+            }
+            pParent->bf_ = pSubR->bf_ = 0;
         }
         void LeftRotateRight(PNode &pParent)
         {
-            ;
+            RotateLeft(pParent->left_);
+            RotateRight(pParent);
         }
         void RightRotateLeft(PNode &pParent)
         {
-            ;
+            RotateRight(pParent->right_);
+            RotateLeft(pParent);
         }
 };
+
+void TestAvl()
+{
+    AVLTree<int,int> t;
+    std::vector<int> v{1,2,3,4,5,6,7,8};   
+    for(int i = 0;i<v.size();++i)
+    {
+        t.insert(v[i],v[i]);
+    }
+}
+
+int main()
+{
+    TestAvl();
+    return 0;
+}
